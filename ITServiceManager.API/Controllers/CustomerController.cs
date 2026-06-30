b7b9ed5ec58d13f6f58dd063aff610721b9393eb
Jakub Strakoš <StrakosJakub@sssvt.cz>
2026-07-01 01:55:14 +0200

    connecting database and first endpoint

---
+++ ITServiceManager.API/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using ITServiceManager.API.Data;
+using ITServiceManager.API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITServiceManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly DatabaseContext database;
+
+        public CustomerController(DatabaseContext database)
+        {
+            this.database = database;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCustomers()
+        {
+            List<CustomerEntity> customers = await database.Customers.ToListAsync();
+
+            return Ok(customers);
+        }
+    }
+}
--- ITServiceManager.API/Program.cs
+++ ITServiceManager.API/Program.cs
@@ -1,4 +1,7 @@
 
+using ITServiceManager.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace ITServiceManager.API
 {
     public class Program
@@ -7,16 +10,21 @@ namespace ITServiceManager.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Services.AddDbContext<DatabaseContext>(options =>
+            {
+                string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+                options.UseMySql(
+                    connection,
+                    ServerVersion.AutoDetect(connection));
+            });
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
