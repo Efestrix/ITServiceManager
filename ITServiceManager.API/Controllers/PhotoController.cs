dc7726849768f1b72ddbf286b4e68b8601c421a6
Jakub Strakoš <StrakosJakub@sssvt.cz>
2026-07-03 18:25:40 +0200

    finished photo endpoints

---
+++ ITServiceManager.API/Controllers/PhotoController.cs
@@ -0,0 +1,56 @@
+﻿using ITServiceManager.API.Dtos.Photo;
+using ITServiceManager.API.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITServiceManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : ControllerBase
+    {
+        private readonly IPhotoService _service;
+        public PhotoController(IPhotoService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _service.GetAllAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreatePhotoDto dto)
+        {
+            PhotoDto createdPhoto = await _service.CreateAsync(dto);
+
+            return CreatedAtAction(nameof(GetAll),
+                new { id = createdPhoto.Id },
+                createdPhoto);
+        }
+
+        [HttpPut("id")]
+        public async Task<IActionResult> Update(int id, UpdatePhotoDto dto)
+        {
+            bool success = await _service.UpdateAsync(id, dto);
+
+            if (!success)
+                return NotFound();
+
+            return NoContent();
+        }
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            bool success = await _service.DeleteAsync(id);
+
+            if (!success)
+                return NotFound();
+
+            return NoContent();
+        }
+    }
+}
---
+++ ITServiceManager.API/Services/PhotoService.cs
@@ -0,0 +1,79 @@
+﻿using ITServiceManager.API.Data;
+using ITServiceManager.API.Dtos.Photo;
+using ITServiceManager.API.Entities;
+using ITServiceManager.API.Mappings;
+using ITServiceManager.API.Services.Interfaces;
+using ITServiceManager.API.Validators;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITServiceManager.API.Services
+{
+    public class PhotoService : IPhotoService
+    {
+        private readonly DatabaseContext _context;
+
+        public PhotoService(DatabaseContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<PhotoDto>> GetAllAsync()
+        {
+            List<PhotoEntity> photos = await _context.Photos.ToListAsync();
+
+            return photos
+                .Select(PhotoMapping.ToDto)
+                .ToList();
+        }
+        public async Task<PhotoDto> CreateAsync(CreatePhotoDto dto)
+        {
+            ValidationResult validation = PhotoValidator.Validate(dto);
+
+            if (!validation.IsValid)
+                throw new ArgumentException(string.Join(Environment.NewLine, validation.Errors));
+
+            PhotoEntity entity = PhotoMapping.ToEntity(dto);
+
+            _context.Photos.Add(entity);
+
+            await _context.SaveChangesAsync();
+
+            return PhotoMapping.ToDto(entity);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            bool photo = await _context.Photos.AnyAsync(p => p.Id == id);
+
+            if (!photo)
+                return false;
+
+            PhotoEntity? entity = await _context.Photos.FindAsync(id);
+
+            if (entity == null)
+                return false;
+
+            _context.Remove(entity);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+        public async Task<bool> UpdateAsync(int id, UpdatePhotoDto dto)
+        {
+            ValidationResult validation = PhotoValidator.Validate(dto);
+
+            if (!validation.IsValid)
+                throw new ArgumentException(string.Join(Environment.NewLine, validation.Errors));
+
+            PhotoEntity? entity = await _context.Photos.FindAsync(id);
+
+            if (entity == null)
+                return false;
+
+            PhotoMapping.UpdateEntity(entity, dto);
+
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
